d071f3310defa7a45a340d85f8eabb25bbe3e3e6
Rishi Chirchi <rishiraj.chirchi@gmail.com>
2024-07-10 12:22:47 +0530

    cors setup

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,10 @@ import os
 import psycopg2 as psy
 from dotenv import load_dotenv
 from newsapi import NewsApiClient
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 #finally lets goooooo
 load_dotenv()
 
